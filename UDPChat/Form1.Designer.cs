02162af7244d26309c939debfed7eed1e4e8ecbb
Vladimir6545 <kvv.login@gmail.com>
2018-06-23 22:28:36 +0300

    first commit

---
+++ UDPChat/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace UDPChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.textBxHistory = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(32, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Имя:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(53, 9);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(80, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBoxMessage.Location = new System.Drawing.Point(238, 12);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(100, 91);
+            this.textBoxMessage.TabIndex = 2;
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(167, 12);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(65, 13);
+            this.labelMessage.TabIndex = 3;
+            this.labelMessage.Text = "Сообщение";
+            // 
+            // textBxHistory
+            // 
+            this.textBxHistory.BackColor = System.Drawing.Color.Aquamarine;
+            this.textBxHistory.Location = new System.Drawing.Point(12, 132);
+            this.textBxHistory.Multiline = true;
+            this.textBxHistory.Name = "textBxHistory";
+            this.textBxHistory.Size = new System.Drawing.Size(423, 118);
+            this.textBxHistory.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(360, 7);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 62);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(108, 62);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 262);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textBxHistory);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.TextBox textBxHistory;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ UDPChat/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using System.Threading;
+
+namespace UDPChat
+{
+    public partial class Form1 : Form
+    {
+
+        bool _done = true; // флаг остановки слушающего потока
+        UdpClient _client; // сокет клиента
+        IPAddress _groupAddress; // групповой адрес рассылки
+        int _localPort; // локальный порт для приема сообщений
+        int _remotePort; // удаленный порт для отправки сообщений
+        int _ttl; // количество маршрутизаторов
+
+        IPEndPoint _remoteEP;
+        UnicodeEncoding _encoding = new UnicodeEncoding();
+        string _name; // имя пользователя в разговоре
+        string _message; // сообщение для отправки
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                NameValueCollection config = ConfigurationManager.AppSettings;
+                _groupAddress = IPAddress.Parse(config["GroupAddress"]);
+                _localPort = int.Parse(config["LocalPort"]);
+                _remotePort = int.Parse(config["RemotePort"]);
+                _ttl = int.Parse(config["TTL"]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _name = textBoxName.Text;
+            textBoxName.ReadOnly = true;
+            try
+            {
+                _client = new UdpClient(_localPort);
+                _client.JoinMulticastGroup(_groupAddress, _ttl);
+                _remoteEP = new IPEndPoint(_groupAddress, _remotePort);
+                Thread receive = new Thread(new ThreadStart(Listener));
+                receive.IsBackground = true;
+                receive.Start();
+                byte[] data = _encoding.GetBytes(_name + "подключился");
+                _client.Send(data, data.Length, _remoteEP);
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Listener()
+        {
+            _done = false;
+            try
+            {
+                while (!_done)
+                {
+                    IPEndPoint point = null;
+                    byte[] buffer = _client.Receive(ref point);
+                    _message = _encoding.GetString(buffer);
+                    this.Invoke(new Action(() => { textBxHistory.Text = $"{DateTime.Now.ToShortTimeString()} { _message } \r\n {textBoxMessage.Text }"; }));
+
+                }
+            }
+            catch(SocketException sex)
+            {
+                if (sex.ErrorCode != 10004)
+                {
+                    MessageBox.Show(sex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            textBxHistory.Text = $"{DateTime.Now.ToShortTimeString()}  {textBoxName.Text}    {textBoxMessage.Text}\r\n{textBxHistory.Text}";
+            try
+            {
+                byte[] data = _encoding.GetBytes($"{_name}:{textBoxMessage.Text}");
+                _client.Send(data, data.Length, _remoteEP);
+                textBoxMessage.Clear();
+                textBoxMessage.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopListener();
+            textBoxName.ReadOnly = false;
+        }
+        void StopListener()
+        {
+            byte[] data = _encoding.GetBytes($"{_name} покинул чат");
+            _client.Send(data, data.Length, _remoteEP);
+            _client.DropMulticastGroup(_groupAddress);
+            _client.Close();
+            _done = true;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_done == false)
+            {
+                StopListener();
+            }
+        }
+    }
+}
